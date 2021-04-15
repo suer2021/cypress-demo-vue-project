5ed872d4d1f83b10713dfc91d70e6861c51bf922
Susanne Erixon <susanne.erixon@skatteverket.se>
2021-04-15 20:24:27 +0200

    Created login-logout test case

--- frontend-tests/cypress/integration/regression-test-suite.spec.js
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -5,6 +5,14 @@ describe('', function(){
     // This is a test case
     it('Perform valid login', function(){
         cy.visit('http://localhost:3000')
+        cy.title().should('include', 'Hotel')
+        cy.contains('Login')
+        cy.get(':nth-child(1) > input').type('tester01')
+        cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+        cy.get('.btn').click()
+        cy.contains('Tester Hotel Overview')
+        cy.get('.user > .btn').click()
+        cy.contains('Login')
     })
 
 })
