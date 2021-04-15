d1b0e1d0971e9a2d120544e29dc080b2208e8b3c
Susanne Erixon <susanne.erixon@skatteverket.se>
2021-04-15 19:29:17 +0200

    Created a .gitignore and test project

---
+++ frontend-tests/cypress/integration/regression-test-suite.spec.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress" />
+
+// This is a test suite
+describe('', function(){
+    // This is a test case
+    it('Perform valid login', function(){
+        cy.visit('http://localhost:3000')
+    })
+
+})
